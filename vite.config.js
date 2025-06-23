597884bf7df28d6e82a226568c5ec909a534605d
Mehdi <semmarmehdi558@gmail.com>
2025-06-22 23:14:23 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ftkubo.github.io/',
+  base: '/ftkubo.github.io/flaggame',
   plugins: [react()],
 })
